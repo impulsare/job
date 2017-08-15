b7ec393b66c77dcef9c9561713ea3932bc6d95a5
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2017-08-15 16:16:58 +0200

    "Bump version 1.0a2"

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.0a1'
+__version__ = '1.0a2'
 
 here = path.abspath(path.dirname(__file__))
 
